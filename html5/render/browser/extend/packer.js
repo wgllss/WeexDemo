410b8ddad6edcef4af4812b222f09b7dac2d0f29
王贵 <283495283@qq.commkdir>
2017-10-09 14:38:46 +0800

    gfg

---
+++ html5/render/browser/extend/packer.js
@@ -0,0 +1,38 @@
+// import components.
+import Image from './components/image'
+import Text from './components/text'
+import Scrollable from './components/scrollable'
+import Slider from './components/slider'
+import Indicator from './components/indicator'
+import Tabheader from './components/tabheader'
+import Input from './components/input'
+import Textarea from './components/textarea'
+import Video from './components/video'
+import Switch from './components/switch'
+import A from './components/a'
+import Embed from './components/embed'
+import Spinner from './components/spinner'
+import Web from './components/web'
+import Neighbor from './components/neighbor'
+import Richtext from './components/richtext'
+
+// import apis.
+import Animation from './api/animation'
+import Clipboard from './api/clipboard'
+import Dom from './api/dom'
+import Event from './api/event'
+import Geolocation from './api/geolocation'
+import GlobalEvent from './api/globalEvent'
+import Meta from './api/meta'
+import Modal from './api/modal'
+import Navigator from './api/navigator'
+import PageInfo from './api/pageInfo'
+import Storage from './api/storage'
+import Stream from './api/stream'
+import Webview from './api/webview'
+import WebSocket from './api/webSocket'
+
+// import npm modules.
+import WeexPicker from 'weex-picker'
+
+export default { Image, Text, Scrollable, Slider, Indicator, Tabheader, Input, Textarea, Video, Switch, A, Embed, Spinner, Web, Neighbor, Richtext, Animation, Clipboard, Dom, Event, Geolocation, GlobalEvent, Meta, Modal, Navigator, PageInfo, Storage, Stream, Webview, WebSocket, WeexPicker }
